8980f495022cf055258f8884e1cd38f6a34fa322
tsorer <139534545+tsorer@users.noreply.github.com>
2023-12-06 11:14:58 +0100

    Day01Reto implementiert

--- Day01Reto/Program.cs
+++ Day01Reto/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Instanz von CalibrationChecker erstellen
+            CalibrationChecker checker = new CalibrationChecker();
+            
+           //Pfad setzen und alle Lines einlesen
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            string path = currentDirectory+"\\data\\data.txt";
+            string[] lines = File.ReadAllLines(path);
+           
+            int sum = checker.CalculateSum(lines);
+
+
+             Console.WriteLine("Die Summe der Calibration-Werte lautet:"+sum);
+
+        } 
+    }
+    
+} 
